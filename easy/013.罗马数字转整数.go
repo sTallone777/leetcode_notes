80155e366363479b11a54739ff7ad726e19cab15
ranbo <45021798+sTallone777@users.noreply.github.com>
2021-03-17 11:50:04 +0900

    Add files via upload

---
+++ easy/013.罗马数字转整数.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Roman struct {
+	val  int
+	next string
+}
+
+func constructor() map[string]Roman {
+	rMap := make(map[string]Roman)
+	rMap["I"] = Roman{
+		val:  1,
+		next: "VX",
+	}
+	rMap["V"] = Roman{
+		val: 5,
+	}
+	rMap["X"] = Roman{
+		val:  10,
+		next: "LC",
+	}
+	rMap["L"] = Roman{
+		val: 50,
+	}
+	rMap["C"] = Roman{
+		val:  100,
+		next: "DM",
+	}
+	rMap["D"] = Roman{
+		val: 500,
+	}
+	rMap["M"] = Roman{
+		val: 1000,
+	}
+	return rMap
+}
+
+func romanToInt(s string) int {
+	rMap := constructor()
+	sum := 0
+	for i := 0; i < len(s); {
+		curr := rMap[string(s[i])]
+		if i < len(s)-1 {
+			if len(curr.next) > 0 {
+				//判断当前字符的下一个是否为需要求差的字符
+				if strings.Index(curr.next, string(s[i+1])) >= 0 {
+					sum += rMap[string(s[i+1])].val - curr.val
+					i += 2
+					continue
+				}
+			}
+		}
+		sum += curr.val
+		i++
+	}
+	return sum
+}
+
+func main() {
+	// s := "III"
+	// s := "IV"
+	// s := "IX"
+	// s := "LVIII"
+	s := "MCMXCIV"
+	fmt.Printf("roman to int: %d\r\n", romanToInt(s))
+}
