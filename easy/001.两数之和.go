3899852be048db6e282dc53548d0db55575432c2
ranbo <45021798+sTallone777@users.noreply.github.com>
2021-04-01 09:55:44 +0900

    Update 001.两数之和.go

--- easy/001.两数之和.go
+++ easy/001.两数之和.go
@@ -1,3 +1,7 @@
+//Because the value to be obtained can be understood as target-x, 
+//repeated calculations can be reduced by using the hashmap. 
+//The answer is when target-x is encountered in the loop.
+
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     retArr := make([]int, 2)
